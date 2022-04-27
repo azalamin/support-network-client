15ed223781c6172f0b3751709083401c2a4e73c2
Al-amin Sheikh <azalamins@gmail.com>
2022-04-27 16:15:25 +0600

    google auth attach to login

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div style={{height: '400px'}} className='d-flex justify-content-center align-items-center'>
+            <h2>Loading......</h2>
+        </div>
+    );
+};
+
+export default Loading;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,49 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
+import auth from '../../firebase.init';
 import logo from "../../logos/logo.png";
+import Loading from "../Loading/Loading";
 import "./Login.css";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  useEffect(() => {
+    if (user) {
+      navigate("/home");
+    }
+  }, [user]);
+  
   return (
     <div>
-      <div className="text-center mt-5">
-        <img
-          className="cursor"
-          onClick={() => navigate("/")}
-          src={logo}
-          alt=""
-        />
-      </div>
-      <div
-        style={{ minHeight: "350px", minWidth: "300px" }}
-        className="w-50 border d-flex justify-content-center align-items-center mx-auto flex-column mt-5"
-      >
-        <h1>Login With</h1>
-        <div className="google-btn mt-3">
-          <div className="google-icon-wrapper">
+      { loading ? <Loading /> :
+        <>
+          <div className="text-center mt-5">
             <img
-              className="google-icon"
-              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+              className="cursor"
+              onClick={() => navigate("/")}
+              src={logo}
               alt=""
             />
           </div>
-          <p className="btn-text">
-            <b>Sign in with google</b>
-          </p>
-        </div>
-        <p className="mt-2">
-          <small>
-            Don’t have an account?{" "}
-            <span
-              onClick={() => navigate("/register")}
-              className="text-primary cursor"
-            >
-              Create an account
-            </span>
-          </small>
-        </p>
-      </div>
+          <div
+            style={{ minHeight: "350px", minWidth: "300px" }}
+            className="w-50 border d-flex justify-content-center align-items-center mx-auto flex-column mt-5"
+          >
+            <h1>Login With</h1>
+            <div onClick={() => signInWithGoogle()} className="google-btn mt-3">
+              <div className="google-icon-wrapper">
+                <img
+                  className="google-icon"
+                  src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                  alt=""
+                />
+              </div>
+              <p className="btn-text">
+                <b>Sign in with google</b>
+              </p>
+            </div>
+            <p className="mt-2">
+              <small>
+                Don’t have an account?{" "}
+                <span
+                  onClick={() => navigate("/register")}
+                  className="text-primary cursor"
+                >
+                  Create an account
+                </span>
+              </small>
+            </p>
+          </div>
+        </>
+      }
     </div>
   );
 };
